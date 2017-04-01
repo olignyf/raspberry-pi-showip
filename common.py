df198b3415a2ed07b05451636efc0fe1b04cb6c4
Francois <frank.quebec@gmail.com>
2017-04-01 03:45:05 +0000

    install.py

---
+++ common.py
@@ -0,0 +1,35 @@
+import select, sys
+import time
+from time import sleep
+from datetime import datetime, timedelta
+import inspect
+import re
+
+
+EOL = "\r\n"
+
+def ExtractBetween(source, start, end):
+    index = source.find(start)
+    if index >= 0:
+        result = source[index+len(start):]
+        index = result.find(end)
+        if index >= 0:
+            return result[0:index]
+    
+    return null
+
+
+def ReplaceBetween(source, start, end, replacement):
+    i_start = source.find(start)
+    if i_start >= 0:
+        trailing = source[i_start+len(start):]
+        i_end = trailing.find(end)
+        print 'i_end:'+str(i_end)
+        if i_end >= 0:
+            print 'end:' + trailing[i_end:]
+            return source[0:i_start+len(start)] + replacement + trailing[i_end:]
+    
+    return null
+
+
+
---
+++ install.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import sys, os, traceback
+from time import sleep
+import time
+import json
+from datetime import datetime, timedelta
+import re
+import signal
+import select # for select.select
+import inspect
+import common
+
+
+EOL = "\r\n"
+
+def signal_handler(signal, frame):
+    print "You pressed exit"
+    exit(0)
+
+def cleanAndExit():
+    sys.exit(0)
+
+# signal handler to handle Ctrl+C and exit server
+signal.signal(signal.SIGINT, signal_handler)
+
+
+# frameAmount is needed by videoClarity, it need to matches the clip length, or smaller if a shorter test is wanted.
+def main():
+    global HTML, allRows, makito_prompt
+    
+    beginTime = time.time()
+    
+    CONFIG = None
+    PATH = "../../.config/lxpanel/LXDE-pi/panels/panel"
+    try:
+        # 
+        CONFIG = open(PATH, 'r').read() 
+    except:
+        print '' # no problem yet
+    
+    if CONFIG == None:
+        try:
+            PATH = "../.config/lxpanel/LXDE-pi/panels/panel"
+            CONFIG = open(PATH, 'r').read() 
+        except:
+            print "Failed to open LXDE-pi/panels/panel"; 
+            return -1
+    
+    MYPLUGIN = 'Plugin {\n  type=showip\n  Config {\n  }\n}'
+    index = CONFIG.find(MYPLUGIN)
+    if index >= 0:
+        print 'Already done'
+        return 0
+    
+    CONFIG = common.ReplaceBetween(CONFIG, 'Plugin {\n  type=volumealsa\n  Config {\n  }\n}\n', 'Plugin', MYPLUGIN+"\n")
+    
+    f = open(PATH, "w")
+    f.write(CONFIG)
+    f.close()
+     
+    # produce report
+    elapsedTime = time.time() - beginTime
+    print "[FINISH] elapsedTime:" + str(elapsedTime)  
+
+if __name__ == '__main__':
+    main()
